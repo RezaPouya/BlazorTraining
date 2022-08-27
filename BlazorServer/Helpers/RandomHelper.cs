ca3fb03a174b846de7c73e3e6d8f3ae3b8a57f1d
r.pouya <r.pouya@R-POUYA>
2022-08-27 17:02:42 +0430

    update

---
+++ BlazorServer/Helpers/RandomHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorServer.Helpers
+{
+    public class RandomHelper
+    {
+        [JSInvokable]
+        public Task<int> MethodFromInstantiateClass(int maxValue)
+        {
+            return Task.FromResult(new Random().Next(maxValue));
+        }
+    }
+}
