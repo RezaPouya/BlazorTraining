7cd8298bbae35f13b10c611c3342f0306efe2ab6
r.pouya <r.pouya@R-POUYA>
2022-08-27 17:00:50 +0430

    JS Interop: Calling C# methods from JavaScript | Blazor Tutorial 6

--- BlazorServer/wwwroot/js/interop.js
+++ BlazorServer/wwwroot/js/interop.js
@@ -16,3 +16,20 @@ function setElementTextBytId(id, text) {
 function focusOnElement(element) {
     element.focus();
 }
+
+
+function generateRandomNumberFromJ(maxIntSize) {
+    console.log('here i am');
+    DotNet.invokeMethodAsync('BlazorServer', 'GenerateRandomNumber', maxIntSize).then(res => {
+        setElementTextBytId('radnomNumberSpan', res);
+    })
+}
+
+
+function giveMeRandomInt(maxIntSize, dotnetInstace) {
+    console.log('giveMeRandomInt');
+    dotnetInstace.invokeMethodAsync('MethodFromInstantiateClass', maxIntSize).then(res => {
+        setElementTextBytId('radnomNumberSpan', res);
+    });
+   
+}
